79c99c63794343aa8ab613ed2299e7f89ea7ec87
SoItGoes33 <smf341@nyu.edu>
2015-09-07 22:15:13 -0700

    adding thumbnail popouts

--- script.js
+++ script.js
@@ -4,4 +4,17 @@
     }
 </script>
 
- 
+ <script type="text/javascript">
+function ShowPopup()
+{
+	var hoverpop = "hover";
+	hp = document.getElementById(hoverpop);
+	hp.style.visibility = "visible";
+}
+
+function HidePopup()
+{
+	hp = document.getElementById("hover");
+	hp.style.visibility = "hidden";
+}
+</script>
